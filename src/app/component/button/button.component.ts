011d702f9a5cab1f1ea399713e9d0e330049453d
hasiitha <bandarahasitha68@gmail.com>
2021-10-16 22:04:22 +0530

    created the button components without the functionalities

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+ @Input() color : string | undefined;
+ @Input() text : string | undefined
   constructor() { }
 
   ngOnInit(): void {
