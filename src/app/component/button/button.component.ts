86c2519a71623006210438e40cf4d56242ad85d0
hasiitha <bandarahasitha68@gmail.com>
2021-10-16 22:18:11 +0530

    btn component btnClick method was called in the header using emmiter

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,13 +8,14 @@ import { Component, OnInit,Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
 
  @Input() color : string | undefined;
- @Input() text : string | undefined
+ @Input() text : string | undefined;
+ @Output() btnClick = new EventEmitter;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onClick() {
-   console.log('clicked')
+   this.btnClick.emit();
   }
 }
