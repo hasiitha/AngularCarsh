c5151db347646e59f5de5cc2e8df046520716aa4
hasiitha <bandarahasitha68@gmail.com>
2022-01-08 16:15:37 +0530

    get method done

--- src/app/service/task-service.service.ts
+++ src/app/service/task-service.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import {Task}from '../Tasks';
-import {Tasks}from '../Mock-Tasks'
-import {Observable,of} from 'rxjs'
+import {Tasks}from '../Mock-Tasks';
+import {Observable} from 'rxjs';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class TaskServiceService {
-
-  constructor() { }
+private  apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http:HttpClient) { }
   getTasks():Observable<Task[]>{
-    const tasks = of(Tasks);
-    return tasks;
-
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
